6251f7d9c9907d9767603a4b94c22e11ccb0fb60
szabotudor <szabo.tudor@gmail.com>
2025-09-17 10:48:41 +0200

    Moved files to jinja named folder

--- android/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![cfg(target_os = "android")]
-
-use {{ template_code_friendly_id }}::*;
-
-#[no_mangle]
-fn android_main(app: AndroidApp) {
-    internal_main(&CommandlineOpts::default(), app).unwrap()
-}
