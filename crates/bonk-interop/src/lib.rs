232c22e3bd1cfe75d3e934b3585220c6e690ecb1
szabotudor <tudor.szabo@traverseresearch.nl>
2026-03-20 14:58:20 +0100

    Add bonk-interop

---
+++ crates/bonk-interop/src/lib.rs
@@ -0,0 +1,4 @@
+// Expose everything from the underlying breda-bonk-interop package from breda
+// Makes it easier to stay forward-compatible with possible additions to the API
+pub use breda_bonk_interop::v0::*;
+
